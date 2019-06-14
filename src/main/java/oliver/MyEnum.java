e6be14fa3315b52e0fa5c67d8e031a41b7372751
Oliver <13164625975@163.com>
2019-06-14 16:05:25 +0800

    忘记提交了

--- src/main/java/oliver/MyEnum.java
+++ src/main/java/oliver/MyEnum.java
@@ -4,6 +4,7 @@ import oliver.OrderType.AA.Color;
 /**
  * @auhor Oliver
  * @date 2019/6/14 15:30
+ * 类里面定义接口
  */
 public class MyEnum {
 
@@ -11,5 +12,9 @@ public class MyEnum {
 
     public MyEnum(Color color){
         this.color = color;
+    }
+
+    public interface Constant{
+        public final String name ="hello";
     }
 }
